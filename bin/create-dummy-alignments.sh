88dc85bec8a12ed7c6ce8ba72349a4d4aed88ea7
Jamie Oaks <joaks1@gmail.com>
2017-01-04 14:01:50 -0600

    Adding scripts to generate dummy alignments.

---
+++ bin/create-dummy-alignments.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+NSPECIES=2
+NGENOMES=10
+NCHARS=1000
+
+i=1
+while [ "$i" -lt 9 ]
+do
+    prefix="c${i}sp"
+    outfile="../alignments/comp0${i}-${NSPECIES}species-${NGENOMES}genomes-${NCHARS}chars.nex"
+    ./generate-dummy-biallelic-alignment.py \
+        --nspecies "$NSPECIES" \
+        --ngenomes "$NGENOMES" \
+        --ncharacters "$NCHARS" \
+        --prefix "$prefix" \
+        > "$outfile"
+    i=`expr $i + 1`
+done
+
+
