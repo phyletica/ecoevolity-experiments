5f2d2bf83cdc10366e2fd585b1494a4dda723469
Jamie Oaks <joaks1@gmail.com>
2017-01-05 01:30:06 -0600

    Adding shell script to create all the ecoevolity scripts.

---
+++ bin/propagate-ecoevolity-sub-scripts.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+i=2
+istart=10
+istop=20
+while [ "$i" -lt 101 ]
+do
+    subnum=$(printf "%06d" $i)
+    outfile="ecoevolity-val-03pairs-dpp-batch01-sub${subnum}.sh"
+    sed -e "s/i=0/i=${istart}/g" -e "s/-lt 10/-lt ${istop}/g" \
+        ecoevolity-val-03pairs-dpp-batch01-sub000001.sh \
+        > "$outfile"
+    chmod +x "$outfile"
+    i=$(expr $i + 1)
+    istart=$(expr $istart + 10)
+    istop=$(expr $istop + 10)
+done
