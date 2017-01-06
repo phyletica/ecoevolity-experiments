b42eaae7a0b227f3d1e84dd8ab769b1f8e138857
Jamie Oaks <joaks1@gmail.com>
2017-01-05 23:11:06 -0600

    Adding paths to project util.

--- bin/project_util.py
+++ bin/project_util.py
@@ -5,6 +5,8 @@ import sys
 
 BIN_DIR = os.path.abspath(os.path.dirname(__file__))
 PROJECT_DIR = os.path.dirname(BIN_DIR)
+SIM_DIR = os.path.join(PROJECT_DIR, 'simulations')
+VAL_DIR = os.path.join(PROJECT_DIR, 'validation')
 
 def main():
     sys.stdout.write("{0}\n".format(PROJECT_DIR))
