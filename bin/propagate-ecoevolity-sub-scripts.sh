b2180052eb7d96162b86475db436f65066b0aaa4
Jamie Oaks <joaks1@gmail.com>
2017-01-06 23:35:00 -0600

    Updating propagate-ecoevolity-sub-scripts.sh
    Now also propagates rj sub scripts.

--- bin/propagate-ecoevolity-sub-scripts.sh
+++ bin/propagate-ecoevolity-sub-scripts.sh
@@ -7,8 +7,12 @@ while [ "$i" -lt 101 ]
 do
     subnum=$(printf "%06d" $i)
     outfile="ecoevolity-val-03pairs-dpp-batch01-sub${subnum}.sh"
+    rjoutfile="ecoevolity-val-03pairs-rj-batch01-sub${subnum}.sh"
     sed -e "s/i=0/i=${istart}/g" -e "s/-lt 10/-lt ${istop}/g" \
         ecoevolity-val-03pairs-dpp-batch01-sub000001.sh \
+        > "$outfile"
+    sed -e "s/i=0/i=${istart}/g" -e "s/-lt 10/-lt ${istop}/g" \
+        ecoevolity-val-03pairs-rj-batch01-sub000001.sh \
         > "$outfile"
     chmod +x "$outfile"
     i=$(expr $i + 1)
