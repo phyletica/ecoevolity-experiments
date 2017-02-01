f80ff7165864bf520a8d446f38da1ee9ec3282cc
Jamie Oaks <joaks1@gmail.com>
2017-01-31 22:06:23 -0600

    Adding simcoevolity qsub scripts for 5-pairs.

---
+++ bin/simcoevolity-val-05pairs-rj-batch01.sh
@@ -0,0 +1,21 @@
+#! /bin/sh
+#PBS -l nodes=1:ppn=1
+#PBS -l walltime=00:30:00
+#PBS -j oe 
+#   #PBS -l jobflags=ADVRES:jro0014_lab.56281
+
+if [ -n "$PBS_JOBNAME" ]
+then
+    source ${PBS_O_HOME}/.bash_profile
+    cd $PBS_O_WORKDIR
+
+    module load gcc/5.3.0
+fi
+
+CFGPATH="../configs/config-5pairs-rj.yml"
+OUTPUTDIR="../simulations/validation/05pairs-rj/batch01"
+SEED="7395376"
+
+mkdir -p "$OUTPUTDIR"
+
+simcoevolity --seed="$SEED" -n 100 -o "$OUTPUTDIR" "$CFGPATH"
