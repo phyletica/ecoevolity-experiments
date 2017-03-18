c9c01322640ef41c84d46dbf1ee0e026ea21af06
Jamie Oaks <joaks1@gmail.com>
2017-03-17 20:02:03 -0500

    Adding walltime option to generate-ecoevolity-qsubs.py

--- bin/generate-ecoevolity-qsubs.py
+++ bin/generate-ecoevolity-qsubs.py
@@ -14,10 +14,10 @@ import project_util
 
 _RNG = random.Random()
 
-def get_pbs_header(restrict_nodes = False):
+def get_pbs_header(restrict_nodes = False, walltime = "5:00:00"):
     s = ("#PBS -l nodes=1:ppn=1\n"
-         "#PBS -l walltime=5:00:00\n"
-         "#PBS -j oe\n")
+         "#PBS -l walltime={0}\n"
+         "#PBS -j oe\n".format(walltime))
     if restrict_nodes:
         s += "#PBS -l jobflags=ADVRES:jro0014_lab.56281\n"
     s += ("\n"
@@ -29,14 +29,14 @@ def get_pbs_header(restrict_nodes = False):
           "fi\n\n")
     return s
 
-def write_qsub(config_path, restrict_nodes = False, rng = _RNG):
+def write_qsub(config_path, restrict_nodes = False, walltime = "5:00:00", rng = _RNG):
     qsub_prefix = os.path.splitext(config_path)[0]
     qsub_path = qsub_prefix + "-qsub.sh"
     if os.path.exists(qsub_path):
         return
     seed = rng.randint(1, 999999999)
     with open(qsub_path, 'w') as out:
-        out.write(get_pbs_header(restrict_nodes))
+        out.write(get_pbs_header(restrict_nodes, walltime))
         out.write("ecoevolity --seed {0} --relax-constant-sites {1}\n".format(
             seed,
             os.path.basename(config_path)))
@@ -66,6 +66,11 @@ def main_cli(argv = sys.argv):
             action = 'store',
             type = int,
             help = 'Random number seed to use for the analysis.')
+    parser.add_argument('--walltime',
+            action = 'store',
+            type = str,
+            default = '5:00:00',
+            help = 'Walltime for qsub scripts.')
     parser.add_argument('--restrict-nodes',
             action = 'store_true',
             help = 'Run only on lab nodes.')
@@ -82,7 +87,8 @@ def main_cli(argv = sys.argv):
 
     for config_path in glob.glob(config_path_pattern):
         write_qsub(config_path = config_path,
-                restrict_nodes = args.restrict_nodes)
+                restrict_nodes = args.restrict_nodes,
+                walltime = args.walltime)
     
 
 if __name__ == "__main__":
