46ab1f482dd94d95a4e15e0bf5b31aa55663599c
Jamie Oaks <joaks1@gmail.com>
2017-05-17 13:37:50 -0500

    Updating script to generat qsub scripts.
    Updating glob so that it does not create a qsub for 'model used for
    simulations' config file.

--- bin/generate-ecoevolity-qsubs.py
+++ bin/generate-ecoevolity-qsubs.py
@@ -83,7 +83,7 @@ def main_cli(argv = sys.argv):
         args.seed = random.randint(1, 999999999)
     _RNG.seed(args.seed)
 
-    config_path_pattern = os.path.join(project_util.VAL_DIR, "*", "*", "*.yml") 
+    config_path_pattern = os.path.join(project_util.VAL_DIR, "*", "*", "simcoevolity-sim-*.yml") 
 
     for config_path in glob.glob(config_path_pattern):
         write_qsub(config_path = config_path,
