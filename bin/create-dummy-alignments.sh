7c34bc862fd867be1150172a1cbd9e42300cebfe
Jamie Oaks <joaks1@gmail.com>
2017-06-22 19:54:25 -0500

    Adding 100k dummy alignments.
    Updating 'create-dummy-alignments.sh' to generate alignments with
    100,000 sites, and adding said alignments.

--- bin/create-dummy-alignments.sh
+++ bin/create-dummy-alignments.sh
@@ -18,4 +18,20 @@ do
     i=`expr $i + 1`
 done
 
+# Generate 100k dummy alignements
+NCHARS=100000
+
+i=1
+while [ "$i" -lt 9 ]
+do
+    prefix="c${i}sp"
+    outfile="../alignments/comp0${i}-${NSPECIES}species-${NGENOMES}genomes-${NCHARS}chars.nex"
+    ./generate-dummy-biallelic-alignment.py \
+        --nspecies "$NSPECIES" \
+        --ngenomes "$NGENOMES" \
+        --ncharacters "$NCHARS" \
+        --prefix "$prefix" \
+        > "$outfile"
+    i=`expr $i + 1`
+done
 
