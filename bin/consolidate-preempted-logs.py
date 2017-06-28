89a95ff68cedd0e055ae1138651208ee5fac93d3
Jamie Oaks <joaks1@gmail.com>
2017-06-28 14:15:48 -0500

    Debugging log consolidation script.

--- bin/consolidate-preempted-logs.py
+++ bin/consolidate-preempted-logs.py
@@ -22,7 +22,7 @@ def line_count(path):
 
 def get_run_number(log_path):
     sim_number_matches = sim_number_pattern.findall(log_path)
-    assert(len(sim_number_matches) == 1)
+    assert len(sim_number_matches) == 1
     sim_number_str = sim_number_matches[0]
     return int(sim_number_str)
 
@@ -36,7 +36,7 @@ def consolidate_preempted_logs(
         batch_dirs = glob.glob(os.path.join(val_sim_dir, "batch*"))
         for batch_dir in sorted(batch_dirs):
             batch_number_matches = batch_number_pattern.findall(batch_dir)
-            assert(len(batch_number_matches) == 1)
+            assert len(batch_number_matches) == 1
             batch_number_str = batch_number_matches[0]
             batch_number = int(batch_number_str)
 
@@ -47,14 +47,14 @@ def consolidate_preempted_logs(
                 sys.stderr.write("WARNING: No log files found for\n"
                         "    Simulation: {0}\n"
                         "    Batch: {1}\n"
-                        "    Target run: {2}\n    Skipping!!".format(
+                        "    Target run: {2}\n    Skipping!!\n".format(
                             sim_name,
                             batch_number,
                             target_run_number))
                 continue
             for posterior_path in sorted(posterior_paths):
                 sim_number_matches = sim_number_pattern.findall(posterior_path)
-                assert(len(sim_number_matches) == 1)
+                assert len(sim_number_matches) == 1
                 sim_number_str = sim_number_matches[0]
                 sim_number = int(sim_number_str)
                 gp = os.path.join(batch_dir,
@@ -62,14 +62,16 @@ def consolidate_preempted_logs(
                                 sim_number_str,
                                 target_run_number))
                 target_state_log_paths = glob.glob(gp)
-                assert(len(target_state_log_paths) == 1, "Multiple matches to {0!r}".format(gp))
+                assert (len(target_state_log_paths) == 1), (
+                        "Multiple matches to {0!r}".format(gp))
                 target_state_log_path = target_state_log_paths[0]
                 gp = os.path.join(batch_dir,
                         "simcoevolity-sim-{0}-config-operator-run-{1}.log*".format(
                                 sim_number_str,
                                 target_run_number))
                 target_op_log_paths = glob.glob(gp)
-                assert(len(target_op_log_paths) == 1, "Multiple matches to {0!r}".format(gp))
+                assert (len(target_op_log_paths) == 1), (
+                        "Multiple matches to {0!r}".format(gp))
                 target_op_log_path = target_op_log_paths[0]
                 state_log_path_pattern = os.path.join(batch_dir,
                         "simcoevolity-sim-{0}-config-state-run-*.log*".format(
@@ -79,20 +81,20 @@ def consolidate_preempted_logs(
                         "simcoevolity-sim-{0}-config-operator-run-*.log*".format(
                                 sim_number_str))
                 op_log_paths = glob.glob(op_log_path_pattern)
-                assert(len(state_log_paths) == len(op_log_paths),
+                assert (len(state_log_paths) == len(op_log_paths)), (
                         "{0} matches for {1!r} and {2} for {3!r}".format(
                             len(state_log_paths),
                             state_log_path_pattern,
                             len(op_log_paths),
                             op_log_path_pattern))
-                assert(target_state_log_path in state_log_paths,
+                assert (target_state_log_path in state_log_paths), (
                         "Target {0!r} not in matches".format(
                                 target_state_log_path))
-                assert(target_op_log_path in op_log_paths,
+                assert (target_op_log_path in op_log_paths), (
                         "Target {0!r} not in matches".format(
                                 target_op_log_path))
                 run_numbers = sorted(get_run_number(p) for p in state_log_paths)
-                assert(run_numbers == sorted(get_run_number(p) for p in op_log_paths))
+                assert (run_numbers == sorted(get_run_number(p) for p in op_log_paths))
                 extra_run_numbers = [rn for rn in run_numbers if rn > target_run_number]
                 if len(extra_run_numbers) < 1:
                     if line_count(target_state_log_path) != number_of_lines:
@@ -101,7 +103,7 @@ def consolidate_preempted_logs(
                                 "    Simulation: {0}\n"
                                 "    Batch: {1}\n"
                                 "    Rep: {2}\n"
-                                "    Target run: {3}\n    Skipping!!".format(
+                                "    Target run: {3}\n    Skipping!!\n".format(
                                         sim_name,
                                         batch_number,
                                         sim_number,
@@ -114,7 +116,7 @@ def consolidate_preempted_logs(
                                 "    Simulation: {0}\n"
                                 "    Batch: {1}\n"
                                 "    Rep: {2}\n"
-                                "    Target run: {3}\n    Skipping!!".format(
+                                "    Target run: {3}\n    Skipping!!\n".format(
                                         sim_name,
                                         batch_number,
                                         sim_number,
@@ -126,7 +128,7 @@ def consolidate_preempted_logs(
                                     sim_number_str,
                                     completed_run_number))
                     completed_state_log_paths = glob.glob(completed_state_log_pattern)
-                    assert(len(completed_state_log_paths) == 1,
+                    assert (len(completed_state_log_paths) == 1), (
                             "Multiple matches to complete state log {0!r}".format(
                                     completed_state_log_pattern))
                     completed_state_log_path = completed_state_log_paths[0]
@@ -135,7 +137,7 @@ def consolidate_preempted_logs(
                                     sim_number_str,
                                     completed_run_number))
                     completed_op_log_paths = glob.glob(completed_op_log_pattern)
-                    assert(len(completed_op_log_paths) == 1,
+                    assert (len(completed_op_log_paths) == 1), (
                             "Multiple matches to complete op log {0!r}".format(
                                     completed_state_log_pattern))
                     completed_op_log_path = completed_op_log_paths[0]
@@ -145,7 +147,7 @@ def consolidate_preempted_logs(
                                 "    Simulation: {0}\n"
                                 "    Batch: {1}\n"
                                 "    Rep: {2}\n"
-                                "    Target run: {3}\n    Skipping!!".format(
+                                "    Target run: {3}\n    Skipping!!\n".format(
                                         sim_name,
                                         batch_number,
                                         sim_number,
