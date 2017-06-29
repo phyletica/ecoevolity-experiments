10307cf6fb40eced5f4808c54eb8ac2561317b35
Jamie Oaks <joaks1@gmail.com>
2017-06-29 12:17:05 -0500

    Adding simcoevolity qsub scripts.
    Adding qsub scripts to generate a 2nd batch of validation simulations.

---
+++ bin/simcoevolity-val-03pairs-dpp-root-0002-500k-batch002-100.sh
@@ -0,0 +1,29 @@
+#! /bin/sh
+#PBS -l nodes=1:ppn=1
+#PBS -l walltime=10:00:00
+#PBS -j oe 
+#PBS -l jobflags=ADVRES:jro0014_lab.56281
+
+username="$USER"
+if [ "$username" == "aubjro" ]
+then
+    module load gcc/6.1.0
+fi
+
+if [ -n "$PBS_JOBNAME" ]
+then
+    source ${PBS_O_HOME}/.bash_profile
+    cd $PBS_O_WORKDIR
+
+    module load gcc/5.3.0
+fi
+
+simname="03pairs-dpp-root-0002-500k"
+cfgpath="../configs/config-${simname}.yml"
+outputdir="../simulations/validation/${simname}/batch002"
+rngseed=731728689
+nreps=100
+
+mkdir -p "$outputdir"
+
+simcoevolity --seed="$rngseed" -n "$nreps" -o "$outputdir" "$cfgpath"
