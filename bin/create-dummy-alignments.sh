99216b3ff5b2438cb8bf89fb620222f034eebd0a
Jamie Oaks <joaks1@gmail.com>
2017-08-01 08:47:35 -0500

    Adding 40,000 site dummy alignments.
    msbayes/dpp-msbayes were struggling to handle 100k datasets, so trying
    40k for the "bake-off".

--- bin/create-dummy-alignments.sh
+++ bin/create-dummy-alignments.sh
@@ -35,3 +35,20 @@ do
     i=`expr $i + 1`
 done
 
+# Generate 40k dummy alignements
+NCHARS=40000
+
+i=1
+while [ "$i" -lt 4 ]
+do
+    prefix="c${i}sp"
+    outfile="../alignments/comp0${i}-${NSPECIES}species-${NGENOMES}genomes-0${NCHARS}chars.nex"
+    ./generate-dummy-biallelic-alignment.py \
+        --nspecies "$NSPECIES" \
+        --ngenomes "$NGENOMES" \
+        --ncharacters "$NCHARS" \
+        --prefix "$prefix" \
+        > "$outfile"
+    i=`expr $i + 1`
+done
+
