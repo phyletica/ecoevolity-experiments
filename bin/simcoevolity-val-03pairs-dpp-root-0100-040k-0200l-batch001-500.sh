5020cab049e6195b125d10336d1ef7cb80b4b6b7
Jamie Oaks <joaks1@gmail.com>
2017-08-01 08:54:32 -0500

    Adding simcoevolity shell script.
    Adding simcoevolity command for simulating 40,000-site datasets.

---
+++ bin/simcoevolity-val-03pairs-dpp-root-0100-040k-0200l-batch001-500.sh
@@ -0,0 +1,30 @@
+#! /bin/sh
+#PBS -l nodes=1:ppn=1
+#PBS -l walltime=10:00:00
+#PBS -j oe 
+#PBS -l jobflags=ADVRES:jro0014_lab.56281
+
+username="$USER"
+if [ "$username" == "aubjro" ]
+then
+    module load gcc/6.1.0
+fi
+
+if [ -n "$PBS_JOBNAME" ]
+then
+    source ${PBS_O_HOME}/.bash_profile
+    cd $PBS_O_WORKDIR
+
+    module load gcc/5.3.0
+fi
+
+locussize="200"
+simname="03pairs-dpp-root-0100-040k"
+cfgpath="../configs/config-${simname}.yml"
+outputdir="../simulations/validation/${simname}-0200l/batch001"
+rngseed=848330470
+nreps=500
+
+mkdir -p "$outputdir"
+
+simcoevolity --seed="$rngseed" -n "$nreps" -l "$locussize" -o "$outputdir" "$cfgpath"
