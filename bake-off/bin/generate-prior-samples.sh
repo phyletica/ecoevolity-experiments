6915b616a2ebd5c52371cb84a7a4ce654bb0ae5a
Jamie R. Oaks <joaks1@gmail.com>
2017-08-18 16:57:41 -0500

    Updating 'bake-off' settings.
    Updating 'bake-off' settings so that msbayes/dpp-msbayes will finish in
    reasonable amount of time.

--- bake-off/bin/generate-prior-samples.sh
+++ bake-off/bin/generate-prior-samples.sh
@@ -11,26 +11,29 @@ then
     cd $PBS_O_WORKDIR
 fi
 
-staging_dir=$(mktemp -d /tmp/output.XXXXXXXXX)
-
-nprocs=20
-nprior=1000000
-batch_size=5000
+nprocs=8
+nprior=500000
+batch_size=6250
 nsums=100000
 sortindex=11
-seed=37851841
+seed=1384268
 
-output_dir="../priors"
+output_dir="../prior"
 if [ ! -d "$output_dir" ]
 then
     mkdir -p $output_dir
 fi
 
-dmc.py --np $nprocs -r 1 -o ../configs/dpp-msbayes.cfg -p ../configs/dpp-msbayes.cfg -n $nprior --prior-batch-size $batch_size --num-standardizing-samples $nsums --sort-index $sortindex --output-dir $output_dir --staging-dir $staging_dir --temp-dir $staging_dir --seed $seed --generate-samples-only 1>generate-prior-samples.sh.out 2>&1
-
-echo "Here are the contents of the local temp directory '${staging_dir}':"
-ls -Fla $staging_dir
-echo 'Removing the local temp directory...'
-rm -r $staging_dir
-echo 'Done!'
-
+dmc.py --np $nprocs \
+    -r 1 \
+    -o ../configs/dpp-msbayes.cfg \
+    -p ../configs/dpp-msbayes.cfg \
+    -n $nprior \
+    --num-posterior-samples $batch_size \
+    --prior-batch-size $batch_size \
+    --num-standardizing-samples $nsums \
+    --sort-index $sortindex \
+    --output-dir $output_dir \
+    --seed $seed \
+    --generate-samples-only \
+    1>generate-prior-samples.sh.out 2>&1
