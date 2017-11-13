0679fce360966bf720b2948396cb424e27d8c457
Jamie R. Oaks <joaks1@gmail.com>
2017-11-12 20:15:35 -0600

    Adding shell scripts for simulating filtered data.

---
+++ bin/simcoevolity-val-03pairs-dpp-root-0100-500k-040singleton-batch001-100.sh
@@ -0,0 +1,21 @@
+#! /bin/sh
+
+if [ -n "$PBS_JOBNAME" ]
+then
+    source ${PBS_O_HOME}/.bash_profile
+    cd $PBS_O_WORKDIR
+
+    module load gcc/5.3.0
+fi
+
+ssprob="0.40"
+ssplabel="$(echo $ssprob | sed -e "s/\.//g")singleton"
+simname="03pairs-dpp-root-0100-500k"
+cfgpath="../configs/config-${simname}.yml"
+outputdir="../simulations/validation/${simname}-${ssplabel}/batch001"
+rngseed=522357854
+nreps=100
+
+mkdir -p "$outputdir"
+
+simcoevolity --seed="$rngseed" --singleton-sample-probability "$ssprob" -n "$nreps" -o "$outputdir" "$cfgpath"
---
+++ bin/simcoevolity-val-03pairs-dpp-root-0100-500k-080singleton-batch001-100.sh
@@ -0,0 +1,21 @@
+#! /bin/sh
+
+if [ -n "$PBS_JOBNAME" ]
+then
+    source ${PBS_O_HOME}/.bash_profile
+    cd $PBS_O_WORKDIR
+
+    module load gcc/5.3.0
+fi
+
+ssprob="0.80"
+ssplabel="$(echo $ssprob | sed -e "s/\.//g")singleton"
+simname="03pairs-dpp-root-0100-500k"
+cfgpath="../configs/config-${simname}.yml"
+outputdir="../simulations/validation/${simname}-${ssplabel}/batch001"
+rngseed=367069470
+nreps=100
+
+mkdir -p "$outputdir"
+
+simcoevolity --seed="$rngseed" --singleton-sample-probability "$ssprob" -n "$nreps" -o "$outputdir" "$cfgpath"
