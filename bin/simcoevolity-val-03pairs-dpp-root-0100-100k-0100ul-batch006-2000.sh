fc44ad3519858b416ddefb9c24e145ab86c67061
Jamie R. Oaks <joaks1@gmail.com>
2017-11-20 16:10:57 -0600

    Adding simcoevolity scripts.
    Adding scripts for simulating "unlinked SNP"-like datasets. I.e.,
    datasets similar to when all sites are thrown away except for one
    variable SNP per locus.

---
+++ bin/simcoevolity-val-03pairs-dpp-root-0100-100k-0100ul-batch006-2000.sh
@@ -0,0 +1,26 @@
+#! /bin/sh
+
+username="$USER"
+if [ "$username" == "aubjro" ]
+then
+    module load gcc/6.1.0
+fi
+
+if [ -n "$PBS_JOBNAME" ]
+then
+    source ${PBS_O_HOME}/.bash_profile
+    cd $PBS_O_WORKDIR
+
+    module load gcc/5.3.0
+fi
+
+locussize="100"
+simname="03pairs-dpp-root-0100-100k"
+cfgpath="../configs/config-${simname}.yml"
+outputdir="../simulations/validation/${simname}-0100ul/batch006"
+rngseed=485261975
+nreps=2000
+
+mkdir -p "$outputdir"
+
+simcoevolity --max-one-variable-site-per-locus --seed="$rngseed" -n "$nreps" -l "$locussize" -o "$outputdir" "$cfgpath"
---
+++ bin/simcoevolity-val-03pairs-dpp-root-0100-500k-0500ul-batch005-100.sh
@@ -0,0 +1,26 @@
+#! /bin/sh
+
+username="$USER"
+if [ "$username" == "aubjro" ]
+then
+    module load gcc/6.1.0
+fi
+
+if [ -n "$PBS_JOBNAME" ]
+then
+    source ${PBS_O_HOME}/.bash_profile
+    cd $PBS_O_WORKDIR
+
+    module load gcc/5.3.0
+fi
+
+locussize="500"
+simname="03pairs-dpp-root-0100-500k"
+cfgpath="../configs/config-${simname}.yml"
+outputdir="../simulations/validation/${simname}-0500ul/batch005"
+rngseed=183944602
+nreps=100
+
+mkdir -p "$outputdir"
+
+simcoevolity --max-one-variable-site-per-locus --seed="$rngseed" -n "$nreps" -l "$locussize" -o "$outputdir" "$cfgpath"
