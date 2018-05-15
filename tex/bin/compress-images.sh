11f9292d4b686a580293ca32503c8c26739ba19b
Jamie R. Oaks <joaks1@gmail.com>
2018-05-15 14:28:36 -0500

    Adding script to compress some vector plots.
    Adding a shell script to resterize and compress some of the plots
    produced by matplotlib. Some of them are large, and some of them are
    rendering differently in different PDF viewers. Rasterizing
    and compressing should help.

---
+++ tex/bin/compress-images.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+paths="$(ls ../../simulations/validation/plots/*nevents*.pdf) $(ls ../../simulations/validation/plots/*scatter*pdf) $(ls ../../bake-off/plots/*.pdf)"
+
+for f in $paths
+do
+    echo "Rasterizing and compressing $f"
+    n=${f/\.pdf/-compressed\.pdf}
+    convert -density 450 -compress jpeg -quality 70 $f $n
+done
+
